6dd24db9d07a928d1755d3d89c41607101b7a6cd
Justine Compagnon <compagno@etud.insa-toulouse.fr>
2015-12-09 13:29:20 +0100

    added class matcher

---
+++ RechercheInformation/src/matcher/Matcher.java
@@ -0,0 +1,28 @@
+package matcher;
+
+import indexation.DatabaseMgmt;
+
+import java.sql.*;
+
+public class Matcher {
+	DatabaseMgmt db;
+
+	public Matcher(DatabaseMgmt db) {
+		this.db = db;
+		// TODO Auto-generated constructor stub
+	}
+
+	//cree un tableau de string avec les mots pertinents
+	public void CleanRequest(String req){
+		
+	}
+	
+	//créé un tableau contenant la liste des documents qui contiennent un  des termes
+	//et son nombre d'occurrence
+	public void MatcherDocWords(){
+		
+	}
+	
+
+	
+}
