48fd64dba7e90b16bce9e284da9d83bf37ec4851
Justine Compagnon <compagno@etud.insa-toulouse.fr>
2015-12-17 12:00:27 +0100

    added performance class

---
+++ RechercheInformation/src/matcher/Performance.java
@@ -0,0 +1,20 @@
+package matcher;
+
+import java.util.ArrayList;
+
+public class Performance {
+	ArrayList<String> corpus;
+	
+	public Performance(ArrayList<String> corpus) {
+		this.corpus = corpus;
+	}
+
+	//calcul le rappel du moteur de recherche en fonction de la reqûete et de notre corpus
+	public void rappel(Requete req){
+	}
+	
+	//calcul la precision du moteur de recherche en fonction de la reqûete et de notre corpus
+	public void precision(Requete req){
+	}
+	
+}
