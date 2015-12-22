11bd849b4a256c33eca146d814a45b4da9f055d3
Julie-R <riviere.julie@hotmail.com>
2015-12-22 23:19:03 +0100

    [v2] faster db

--- RechercheInformation_V2/src/Main.java
+++ RechercheInformation_V2/src/Main.java
@@ -17,63 +17,73 @@ public class Main {
 	static int idDoc = 1 ; 
 	static ArrayList<Request> reqs  = new ArrayList<Request>();
 	static ArrayList<String> docs = new ArrayList<String>();
-	
-	
-	
-	  public static void main( String args[] ){
-		  db.loadDB();
-		 // db.createTable();
-		  
-		  /*db.insertWordOrDoc("WORDS", "coucou");
-		  db.insertWordOrDoc("DOCS", "D2");
-		  db.insertWordOrDoc("WORDS", "chat");
-		  db.insertWordOrDoc("DOCS", "D1");
-		  System.out.println("insertIndexation(1, 1)");
-		  db.insertIndexation(1, 1);
-		  System.out.println("insertIndexation(1, 2)");
-		  db.insertIndexation(1, 2);
-		  System.out.println("insertIndexation(2, 2)");
-		  db.insertIndexation(2, 2);
-		  System.out.println("***");
-		  System.out.println(db.wordExists("coucou"));
-		  System.out.println(db.wordExists("pizza"));
-		  System.out.println(db.getID("WORDS", "coucou"));*/
-		  //db.insertIndexation(23, 23);
-		  //matcher.setDatabaseMgmt(db);
-		  //matcherTest();
-		  
 
-		  //On charge le Corpus
-		  docs = FileManager.listerRepertoire(Cst.docsPath);
-		  
-		  //On charge les requetes
-		  reqs = Request.createListReq(Cst.reqsPath);
-		  
-		  //On initialise le matcher avec la liste des requêtes et des docs
-		  matcher = new Matcher(reqs, db);
-		  
-		  Performance p = new Performance(docs);
-		  //On met en route le matcher
-		  matcher.matchAll(docs);
-		  for (Request r : reqs){
-			  System.out.println(r);
-			  System.out.println(p.rappel(r, 5)+p.rappel(r, 15)+p.rappel(r, 25)+p.precision(r, 5)+p.precision(r, 15)+p.precision(r, 25));
-		  }
-		  
-		
-		  db.closeDB();
-	  }
-	  
-	  /*public static void matcherTest(){
-		  ArrayList<String> docs = new ArrayList<String>();
-		  docs = FileManager.listerRepertoire("/home/compagnon/Documents/5A/RI/RechercheInfos/RechercheInformation/CORPUS/CORPUS");
-		  //matcher.CleanRequest("Le compagnon de sa soeur est le coucou de sa femme");
-		  ArrayList<String> terms = matcher.CleanRequest("personnes, Intouchables");
-		  //matcher.SumTermFrequency( terms, "D15.html");
-		  Object[] mapTrier = matcher.MatcherDocWords(terms, docs);
-		  for (Object e : mapTrier) {
-				System.out.println(((Map.Entry<String, Integer>) e).getKey() + " : "
-						+ ((Map.Entry<String, Integer>) e).getValue());
-			}
-	  }*/
+	public void testDB() {
+		db.loadDB();
+		db.createTable();
+		db.setAutoCommit(false);
+		db.insertWordOrDoc("WORDS", "coucou");
+		db.insertWordOrDoc("DOCS", "D2");
+		db.insertWordOrDoc("WORDS", "chat");
+		db.insertWordOrDoc("DOCS", "D1");
+		System.out.println("insertIndexation(1, 1)");
+		db.insertIndexation(1, 1);
+		System.out.println("insertIndexation(1, 2)");
+		db.insertIndexation(1, 2);
+		System.out.println("insertIndexation(2, 2)");
+		db.insertIndexationWithFrequency(2, 2, 2);
+		db.commit();
+		System.out.println("***");
+		System.out.println(db.wordExists("coucou"));
+		System.out.println(db.wordExists("pizza"));
+		System.out.println(db.getID("WORDS", "coucou"));
+	}
+
+	/*
+	public static void matcherTest(){
+		ArrayList<String> docs = new ArrayList<String>();
+		docs = FileManager.listerRepertoire("/home/compagnon/Documents/5A/RI/RechercheInfos/RechercheInformation/CORPUS/CORPUS");
+		//matcher.CleanRequest("Le compagnon de sa soeur est le coucou de sa femme");
+		ArrayList<String> terms = matcher.CleanRequest("personnes, Intouchables");
+		//matcher.SumTermFrequency( terms, "D15.html");
+		Object[] mapTrier = matcher.MatcherDocWords(terms, docs);
+		for (Object e : mapTrier) {
+			System.out.println(((Map.Entry<String, Integer>) e).getKey() + " : "
+					+ ((Map.Entry<String, Integer>) e).getValue());
+		}
+	}
+
+
+	public void testMatcher() {
+		matcher.setDatabaseMgmt(db);
+		matcherTest();
+	}
+	 */
+
+
+	public static void main( String args[] ){
+		//FillDatabase fdb = new FillDatabase();
+		//fdb.fillDatabaseWithAllFiles(false);
+		db.loadDB();
+		//On charge le Corpus
+		docs = FileManager.listerRepertoire(Cst.docsPath);
+
+		//On charge les requetes
+		reqs = Request.createListReq(Cst.reqsPath);
+
+		//On initialise le matcher avec la liste des requêtes et des docs
+		matcher = new Matcher(reqs, db);
+
+		Performance p = new Performance(docs);
+		//On met en route le matcher
+		matcher.matchAll(docs);
+		for (Request r : reqs){
+			System.out.println(r);
+			System.out.println(p.rappel(r, 5)+p.rappel(r, 15)+p.rappel(r, 25)+p.precision(r, 5)+p.precision(r, 15)+p.precision(r, 25));
+		}
+
+		db.closeDB();
+	}
+
 }
+
